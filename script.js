cfb0bf5be1cca75eea74c43ab098061ae512c1cb
tristanmatthew <77029216+tristanmatthew@users.noreply.github.com>
2021-02-03 21:58:50 -0500

    Add files via upload
    conversion working, basic styling.

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-const currencies = [
-  'EUR',
-  'CAD',
-  'HKD',
-  'ISK',
-  'PHP',
-  'DKK',
-  'HUF',
-  'CZK',
-  'AUD',
-  'RON',
-  'SEK',
-  'IDR',
-  'INR',
-  'BRL',
-  'RUB',
-  'HRK',
-  'JPY',
-  'THB',
-  'CHF',
-  'SGD',
-  'PLN',
-  'BGN',
-  'TRY',
-  'CNY',
-  'NOK',
-  'NZD',
-  'ZAR',
-  'USD',
-  'MXN',
-  'ILS',
-  'GBP',
-  'KRW',
-  'MYR'
-]
+const fromCurrencyEl = document.getElementById('fromCurrency');
+const fromAmountEl = document.getElementById('fromAmount');
+const toCurrencyEl = document.getElementById('toCurrency');
+const toAmountEl = document.getElementById('toAmount')
+const rateEl = document.getElementById('rate');
+const conversion = document.querySelector('.conversion');
 
+console.log(fromCurrencyEl);
+console.log(fromAmountEl);
+console.log(toCurrencyEl);
+console.log(toAmountEl);
+console.log(rateEl);
+console.log(conversion);
+
+fromCurrencyEl.addEventListener('change', calculate);
+fromAmountEl.addEventListener('input', calculate);
+toCurrencyEl.addEventListener('change', calculate);
+toAmountEl.addEventListener('input', calculate);
+
+conversion.addEventListener('click', () => {
+  const temp = fromCurrencyEl.value;
+  fromCurrencyEl.value = toCurrencyEl.value;
+  toCurrencyEl.value = temp;
+  calculate();
+});
+
+
+function calculate() {
+  const fromCurrency = fromCurrencyEl.value;
+  const toCurrency = toCurrencyEl.value;
+
+  fetch(`https://api.exchangeratesapi.io/latest?base=${fromCurrency}`)
+    .then(resp => resp.json())
+    .then(resp => {
+      const rate = resp.rates[toCurrency];
+      rateEl.innerText = `1 ${fromCurrency} = ${rate} ${toCurrency}`
+      toAmountEl.value = (fromAmountEl.value * rate).toFixed(2);
+    })
+}
+
+calculate();
